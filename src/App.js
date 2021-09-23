5ca0bfea513cbcecfc6b77b4412788d66439fc2f
Schmidt1697 <anna.schmidt1697@gmail.com>
2021-09-23 15:27:01 -0600

    navbar
    started navbar.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
 class App extends Component {
 	render() {
-		const name = "Nana Pig";
-
-		const loading = false;
-
-		if (loading) {
-			return <h4>Loading. . .</h4>;
-		}
 		return (
 			<div className="App">
-				{loading ? <h4>Loading...</h4> : <h4>Hello {name}</h4>}
+				<Navbar icon="fab fa-github" title="Github Finder" />
 			</div>
 		);
 	}
